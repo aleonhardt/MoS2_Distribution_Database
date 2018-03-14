fb7e9c92e0dec40cd71b8ddde2ffa10377f5b95a
Alessandra Leonhardt (----) <leonha54@imec.be>
2018-03-14 15:42:16 +0100

    Parameter calculations and labels

--- MoS2_Analysis_Module.py
+++ MoS2_Analysis_Module.py
@@ -9,18 +9,19 @@ This is a temporary script file.
 import os
 import matplotlib.pyplot as plt
 import fnmatch
+import math
 import numpy as np
 
 class MoS2_Analysis:
     
     def __init__(self, path):     
-        print('Any')
         self.path=path
         self.MaxCurrent=[]
         self.Length=[]
         self.Width=[]
         
         self.loadData(self.path)
+        self.calculateParameters()
         self.plotDistributions()
     
     def loadData(self,root):
@@ -33,21 +34,30 @@ class MoS2_Analysis:
                 deviceWidth=Data[1]
                 deviceCurrent=Data[2]
 
-                self.Width.append(deviceWidth)
-                self.Length.append(deviceLength)
-                self.MaxCurrent.append(deviceCurrent)
-    
-        print(len(self.MaxCurrent[0]))
+                for i in range(0, len(deviceCurrent)):
+                    if(deviceCurrent[i]>0):
+                        self.MaxCurrent.append(deviceCurrent[i])
+                        self.Width.append(deviceWidth[i])
+                        self.Length.append(deviceLength[i])
+                
     
     def createDistributions():
         print('Any')
     
-    def calculateStdDev():
-        print('Any')
+    def calculateParameters(self):
+        logCurrent=[math.log(y) for y in self.MaxCurrent]
+        self.stdDev=np.std(logCurrent)
+        print(self.stdDev)
+        self.mean=np.mean(self.MaxCurrent)
+        print(self.mean)
+        self.coeffVariance=np.sqrt(np.exp(self.stdDev*self.stdDev)-1)
     
     def plotDistributions(self):
         # the histogram of the data
         plt.close('all')
-        plt.hist(self.MaxCurrent[0], bins=np.logspace(np.log10(1e-13),np.log10(1e-5), 50))
+        plt.hist(self.MaxCurrent, bins=np.logspace(np.log10(1e-13),np.log10(1e-6), 50), edgecolor='black', linewidth=1.2)
         plt.gca().set_xscale("log")
+        plt.title('coefficient of variance (log-normal): '+str("{0:.2f}".format(self.coeffVariance)+'%'))
+        plt.ylabel('Frequency')
+        plt.xlabel('Max Current (A/$\mu$m)')
         plt.show()
